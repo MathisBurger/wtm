47c61b8a9222fa9862bbf1d58d8973e32838e0ed
Mathis Burger <handymathis@gmail.com>
2024-05-06 14:23:26 +0200

    [FEAT] Added skeleton of check in and check out action

---
+++ src/Controller/CheckInOutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Controller for handling check in and out actions
+ */
+class CheckInOutController extends AbstractController
+{
+
+    /**
+     * Performs check in action
+     */
+    #[Route('/api/v1/check-in/{username}', name: 'api_v1_check_in')]
+    public function checkIn(string $int): Response
+    {
+        return new Response();
+    }
+
+    /**
+     * Performs check out action
+     */
+    #[Route('/api/v1/check-out/{username}', name: 'api_v1_check_out')]
+    public function checkOut(string $int): Response
+    {
+        return new Response();
+    }
+
+
+}
